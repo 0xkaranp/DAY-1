62723482f533995c649bcb1987ec2316e63488d9
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2022-06-14 22:30:19 -0700

    arrays/condtion.cpp

---
+++ conditonal.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+  variable = (condition) ? true : false  //found error in this program
+   
+  int largestNum = (5>2) ? 5 : 2;
+
+
+
+  int myfavnums[5];
+
+  int badnums[5]={ 4, 13, 14, 24, 34};  // shows ouput as 4
+ 
+  cout << "badnums number 1:"<< badnums[0] << endl;
+
+  return 0;
+}
